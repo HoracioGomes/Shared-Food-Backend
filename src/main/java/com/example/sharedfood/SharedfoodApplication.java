6ed214c0e216db2a932f4d6e5d8771c476ee1900
Horacio <oicaroh64@gmail.com>
2023-09-05 21:18:17 -0300

    Initial commit

---
+++ src/main/java/com/example/sharedfood/SharedfoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharedfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sharedfood/SharedfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharedfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
